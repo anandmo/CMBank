cbff5c696226ca06742651811ce7f4b83f5ac1d1
Anand Mohan <im.anand.m@gmail.com>
2021-01-07 22:33:43 +0530

    Added custom user detail service class to find user based on email id using user repository class

---
+++ src/main/java/com/cmbank/service/CustomUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.cmbank.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.cmbank.authentication.CustomUserDetails;
+import com.cmbank.model.User;
+import com.cmbank.repository.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
