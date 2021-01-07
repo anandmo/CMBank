99c15d906624c494f89c201699b21bbb54d83865
Anand Mohan <im.anand.m@gmail.com>
2021-01-07 16:36:59 +0530

    Created initial user Model class

--- src/main/java/com/cmbank/model/User.java
+++ src/main/java/com/cmbank/model/User.java
@@ -1,5 +1,29 @@
 package com.cmbank.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false, unique = true, length = 45)
+	private String email;
+
+	@Column(nullable = false, length = 64)
+	private String password;
+
+	@Column(name = "first_name", nullable = false, length = 20)
+	private String firstName;
+
+	@Column(name = "last_name", nullable = false, length = 20)
+	private String lastName;
 }
