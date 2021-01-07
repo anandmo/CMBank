024403fc8d1addf505384d8555c3b57916cb75ab
Anand Mohan <im.anand.m@gmail.com>
2021-01-07 21:36:57 +0530

    Added user registration form using HTML 5 and Bootstrap

--- src/main/java/com/cmbank/controller/AppController.java
+++ src/main/java/com/cmbank/controller/AppController.java
@@ -1,9 +1,13 @@
 package com.cmbank.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.cmbank.model.User;
 import com.cmbank.repository.UserRepository;
 
 @Controller
@@ -15,5 +19,23 @@ public class AppController {
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
+	}
+
+	@GetMapping("/register")
+	public String showRegistrationForm(Model model) {
+		model.addAttribute("user", new User());
+
+		return "signup_form";
+	}
+
+	@PostMapping("/process_register")
+	public String processRegister(User user) {
+		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+		String encodedPassword = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodedPassword);
+
+		userRepo.save(user);
+
+		return "register_success";
 	}
 }
